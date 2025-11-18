70727baf7603a5124bb8b5682d595db1dbe8d17b
Sadat-SH <sadachok06@gmail.com>
2025-11-18 19:51:25 +0600

    Использованы геттеры для приватных атрибутов

--- homework3.2.py
+++ homework3.2.py
@@ -7,6 +7,16 @@ class Person:
         self.__occupation = occupation 
         self.__higher_education = higher_education 
 
+    # Геттеры 
+    def get_birth_date(self):
+        return self.__birth_date
+
+    def get_occupation(self):
+        return self.__occupation
+
+    def has_higher_education(self):
+        return self.__higher_education
+
     @property
     def age(self):
         birth = datetime.strptime(self.__birth_date, "%d.%m.%Y")
@@ -25,9 +35,9 @@ class Classmate(Person):
         self.group_name = group_name
 
     def introduce(self):
-        edu_text = "У меня есть высшее образование." if self._Person__higher_education else "У меня нет высшего образования."
+        edu_text = "У меня есть высшее образование." if self.has_higher_education() else "У меня нет высшего образования."
         print(f"Привет, меня зовут {self.name}. "
-              f"Моя профессия {self._Person__occupation}. "
+              f"Моя профессия {self.get_occupation()}. "
               f"Я учился в группе {self.group_name}. {edu_text}")
 
 
@@ -37,10 +47,11 @@ class Friend(Person):
         self.hobby = hobby
 
     def introduce(self):
-        edu_text = "У меня есть высшее образование." if self._Person__higher_education else "У меня нет высшего образования."
+        edu_text = "У меня есть высшее образование." if self.has_higher_education() else "У меня нет высшего образования."
         print(f"Привет, меня зовут {self.name}. "
-              f"Моя профессия {self._Person__occupation}. "
+              f"Моя профессия {self.get_occupation()}. "
               f"Мое хобби {self.hobby}. {edu_text}")
+
 
 cl1 = Classmate("Иван", "20.02.2000", "студент", True, "11D")
 cl1.introduce()
@@ -50,3 +61,6 @@ fr1.introduce()
 
 print("\nВозраст Айбека:", fr1.age)
 print("Возраст Ивана:", cl1.age)
+
+    def __str__(self):
+        return f"{self.get_value()} {self.get_unit()}"
